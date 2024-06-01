afa97d6aa5eaa51916df3f58bc48035835105378
Rastele <unrastele@gmail.com>
2024-06-01 22:27:42 +0200

    Added dropdown menu in top bar UI

--- frontend/src/components/TopBar/TopBar.js
+++ frontend/src/components/TopBar/TopBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './TopBar.css';
@@ -7,7 +7,22 @@ import profilePic from '../../assets/placeholder_profile.png';
 
 const TopBar = () => {
     const [userData, setUserData] = useState(null);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
     const navigate = useNavigate();
+    const dropdownRef = useRef(null);
+
+    const handleLogout = () => {
+        localStorage.removeItem('Authorization');
+        navigate('/logout');
+    };
+
+    const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setDropdownOpen(false);
+        }
+    };
 
     useEffect(() => {
         const token = localStorage.getItem('Authorization');
@@ -21,6 +36,13 @@ const TopBar = () => {
                 console.error(error);
                 navigate('/login');
             });
+    },[]);
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
     }, []);
 
     return (
@@ -30,10 +52,15 @@ const TopBar = () => {
             </div>
             <div className="user-info">
                 <span className="user-points">Points: {userData ? userData.points : ''}</span>
-                <a href="#" className="user-nickname">
+                <div className="user-nickname" onClick={toggleDropdown} ref={dropdownRef}>
                     <img src={profilePic} alt="Profile" />
                     {userData ? userData.username : ''}
-                </a>
+                    {dropdownOpen &&
+                        <div className="dropdown-menu">
+                            <div className="dropdown-item" onClick={handleLogout}>Logout</div>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
     );
