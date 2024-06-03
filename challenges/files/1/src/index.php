db2da2a9c49cfb92033cda7e9995efc2eadc6850
Rastele <unrastele@gmail.com>
2024-06-03 14:02:58 +0200

    Created example challenge

---
+++ challenges/files/1/src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>R0b0ts</title>
+</head>
+<body>
+    <h1>ABC Company website - in development...</h1>
+	<img src="robots.jfif" alt="Image of robots" width="512" height="512">
+	<!-- 
+	Note from manager: be sure to hide our secret files. Don't let Googlebot find it.
+	-->
+</body>
+</html>
---
+++ challenges/files/1/src/secret.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>R0b0ts</title>
+</head>
+<body>
+    <h1>2b0000e00af96859b4c8acc0af88b5f2</h1>
+</body>
+</html>
